f318fc89d30c5e4155e5c1e9f1d350d861ab1330
Daniel Libatique <daniel.libatique@gmail.com>
2020-07-05 17:12:37 -0400

    finished templates module

--- 14-Django_Level_One/first_project/first_app/views.py
+++ 14-Django_Level_One/first_project/first_app/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    my_dict = {'insert_me': 'Now I am coming from first_app/index.html!'}
+
+    return render(request, 'first_app/index.html', context=my_dict)
