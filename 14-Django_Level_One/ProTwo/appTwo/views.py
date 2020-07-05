b48dd918af4939f2c5744e453eed8c987f3aafde
Daniel Libatique <daniel.libatique@gmail.com>
2020-07-05 16:13:26 -0400

    started django module one

--- 14-Django_Level_One/ProTwo/appTwo/views.py
+++ 14-Django_Level_One/ProTwo/appTwo/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("<em>My Second Project</em>")
 
-def help(request):
-    helpdict = {'help_insert':'HELP PAGE'}
-    return render(request,'appTwo/help.html',context=helpdict)
+def index(request):
+    return HttpResponse('<em>My Second App</em>')
