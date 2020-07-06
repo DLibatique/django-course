2378cae151680a1f640235a2a806f1c82ad2c812
Daniel Libatique <daniel.libatique@gmail.com>
2020-07-06 12:52:42 -0400

    began MTV modules

--- 15-Django_Level_Two/project/first_app/views.py
+++ 15-Django_Level_Two/project/first_app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from first_app.models import AccessRecord, Topic, Webpage
 
 # Create your views here.
 def index(request):
 
-    my_dict = {
-        'insert_content': "HELLO, I'M FROM FIRST_APP"
+    webpages_list = AccessRecord.objects.order_by('date')
+
+    date_dict = {
+        'access_records': webpages_list
     }
 
-    return render(request, 'first_app/index.html', context = my_dict)
+    return render(request, 'first_app/index.html', context = date_dict)
